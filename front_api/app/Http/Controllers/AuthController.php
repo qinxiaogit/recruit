1004e895d32a06df7cfb01ea73d62c6beedafbde
owlet <qinxiao@zhicloud.com>
2023-08-03 23:11:36 +0800

    debug

--- front_api/app/Http/Controllers/AuthController.php
+++ front_api/app/Http/Controllers/AuthController.php
@@ -145,7 +145,7 @@ class AuthController extends Controller
         $age = $request->post('age');
         $nickname = $request->post('nickname');
         if (!empty($age)) {
-            $birthday = ( (intval(date("Y")) - $age)."-01-01");
+            $birthday = date("Y-m-d",time()-$age*24*86400);
         }
 
 
